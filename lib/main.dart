578eec24d68be553bb4fd4cb66f4ac81abd55edb
Omkar Ubale <oubale@gmail.com>
2019-09-01 04:54:53 +0530

    LEARNING: Completed the 2nd section of the course

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,75 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-@override
+  @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return new _MyAppState();
   }
 }
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion(){
+  final _questions = [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Blue', 'score': 5},
+        {'text': 'Red', 'score': 1}
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Tiger', 'score': 20},
+        {'text': 'Dog', 'score': 10},
+        {'text': 'Wolf', 'score': 5}
+      ]
+    },
+    {
+      'questionText': 'Who\'s your favorite actress?',
+      'answers': [
+        {'text': 'Jennifer Aniston', 'score': 10},
+        {'text': 'Salma Hayek', 'score': 10},
+        {'text': 'Saorse Ronan', 'score': 10}
+      ]
+    }
+  ];
+  void _answerQuestion(int score) {
     setState(() {
+      _totalScore += score;
       _questionIndex = _questionIndex + 1;
     });
-    print(_questionIndex);
+  }
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = 
-    [
-      'What\'s your facvorite color?',
-      'What\'s your favorite animal?'
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(questions[_questionIndex]),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: _answerQuestion,
-            )
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                questions: _questions,
+                questionIndex: _questionIndex,
+                answerQuestion: _answerQuestion,
+              )
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
---
+++ lib/result.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetQuiz;
+
+  Result(this.resultScore, this.resetQuiz);
+
+  String get resultPhrase {
+    return resultScore.toString() + ' is your final score!';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          FlatButton(
+            child: Text('Restart Quiz!'),
+            textColor: Colors.blue,
+            onPressed: resetQuiz,
+          )
+        ],
+      ),
+    );
+  }
+}
